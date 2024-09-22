643c9bcc29a3b6cf6bb2feb86fcd90498ddcda03
FLLess <fllessive@gmail.com>
2024-09-23 01:24:07 +0800

    删除一些无用代码

--- src/main/java/tech/flless/huhubot/config/XiaoAiConfig.java
+++ src/main/java/tech/flless/huhubot/config/XiaoAiConfig.java
@@ -6,28 +6,28 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author elastic chow
  * @date 24/7/2023
  */
-//@ConfigurationProperties(prefix = "xiaoai")
-//@Deprecated
-//public class XiaoAiConfig {
-//
-//    public static String deviceId;
-//    public static String userId;
-//    public static String serviceToken;
-//    public static String securityToken;
-//
-//    public void setDeviceId(String deviceId) {
-//        XiaoAiConfig.deviceId = deviceId;
-//    }
-//
-//    public void setUserId(String userId) {
-//        XiaoAiConfig.userId = userId;
-//    }
-//
-//    public void setServiceToken(String serviceToken) {
-//        XiaoAiConfig.serviceToken = serviceToken;
-//    }
-//
-//    public void setSecurityToken(String securityToken) {
-//        XiaoAiConfig.securityToken = securityToken;
-//    }
-//}
+@ConfigurationProperties(prefix = "xiaoai")
+@Deprecated
+public class XiaoAiConfig {
+
+    public static String deviceId;
+    public static String userId;
+    public static String serviceToken;
+    public static String securityToken;
+
+    public void setDeviceId(String deviceId) {
+        XiaoAiConfig.deviceId = deviceId;
+    }
+
+    public void setUserId(String userId) {
+        XiaoAiConfig.userId = userId;
+    }
+
+    public void setServiceToken(String serviceToken) {
+        XiaoAiConfig.serviceToken = serviceToken;
+    }
+
+    public void setSecurityToken(String securityToken) {
+        XiaoAiConfig.securityToken = securityToken;
+    }
+}
