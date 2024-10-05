85f36a5f3cb444f7bc46bd8b3c9ad5368d7d4591
FLLess <64878354+fllesser@users.noreply.github.com>
2024-10-06 03:56:56 +0800

    Update Bot.java

--- src/main/java/tech/flless/huhubot/adapters/onebot/v11/bot/Bot.java
+++ src/main/java/tech/flless/huhubot/adapters/onebot/v11/bot/Bot.java
@@ -57,11 +57,11 @@ public class Bot {
         try {
             this.session.sendMessage(new TextMessage(text));
         } catch (IOException e) {
-            log.info("{}[huhubot]-ws->[onebotv11] [{}]{}, exception[{}]{}",
+            log.info("{}[hb]-ws->[ob][{}]{}, exception[{}]{}",
                     ANSI.RED, selfId, text, e.getMessage(), ANSI.RESET);
         }
         if (!requestBox.getAction().equals("set_group_card")) {
-            log.info("[huhubot]-ws->[onebotv11] [{}]{}", selfId, text);
+            log.info("[hb]-ws->[ob][{}]{}", selfId, text);
         }
     }
 
