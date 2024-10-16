382e71a94a0e254f8080048e3b50ca5e3d6d9917
FLLess <64878354+fllesser@users.noreply.github.com>
2024-10-17 02:01:42 +0800

    Update AIPlugin.java

--- src/main/java/tech/flless/huhubot/plugins/ai/AIPlugin.java
+++ src/main/java/tech/flless/huhubot/plugins/ai/AIPlugin.java
@@ -97,7 +97,7 @@ public class AIPlugin {
             String audioUrl = reechoClient.generate(voiceMap.get(roleName), text);
             event.reply(MessageSegment.record(audioUrl));
         } catch (ExecutionException | InterruptedException | TimeoutException e) {
-            throw new FinishedException("未知错误: " + e.getClass().toString());
+            throw new FinishedException("请求超时");
         } finally {
             try {
                 bot.deleteMsg(messageId.get());
