e62481c8fa706a57eb41353f7adfb14bbf15297d
fllesser <19550518zrf@gmail.com>
2024-10-20 19:09:45 +0800

    add:适配napcat的新增api

--- src/main/java/com/github/huhubot/plugins/ai/reecho/ReechoUtil.java
+++ src/main/java/com/github/huhubot/plugins/ai/reecho/ReechoUtil.java
@@ -1,6 +1,7 @@
 package com.github.huhubot.plugins.ai.reecho;
 
 import com.github.huhubot.plugins.ai.reecho.entity.resp.RoleList;
+import com.github.huhubot.utils.IocUtil;
 
 import java.util.List;
 import java.util.Map;
@@ -9,27 +10,35 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ReechoUtil {
 
     //name:id
-    private static final Map<String, String> RoleMap = new ConcurrentHashMap<>();
+    private static final Map<String, String> roleMap = new ConcurrentHashMap<>();
+
+    static {
+        ReechoClient reechoClient = IocUtil.getBean(ReechoClient.class);
+        if (roleMap.isEmpty()) {
+            RoleList roleList = reechoClient.getVoiceList();
+            update(roleList);
+        }
+    }
 
     public static boolean contains(String name) {
-        return RoleMap.containsKey(name);
+        return roleMap.containsKey(name);
     }
 
     public static void update(RoleList roleList) {
-        RoleMap.clear();
-        roleList.getData().forEach(role -> RoleMap.put(role.getName(), role.getId()));
+        roleMap.clear();
+        roleList.getData().forEach(role -> roleMap.put(role.getName(), role.getId()));
     }
 
     public static String get(String key) {
-        return RoleMap.get(key);
+        return roleMap.get(key);
     }
 
     public static boolean isEmpty() {
-        return RoleMap.isEmpty();
+        return roleMap.isEmpty();
     }
 
     public static String randId() {
-        List<String> ids = RoleMap.values().stream().toList();
+        List<String> ids = roleMap.values().stream().toList();
         return ids.get((int) (Math.random() * ids.size()));
     }
 
